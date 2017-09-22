b5c86fc586b3d6915762ed620a99dc47ba15f3d8
Suyeol Jeon <devxoul@gmail.com>
2017-09-22 17:51:56 +0900

    Update Package.swift to V4

--- Package.swift
+++ Package.swift
@@ -1,24 +1,19 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
-import Foundation
 import PackageDescription
-
-var dependencies: [Package.Dependency] = [
-  .Package(url: "https://github.com/Moya/Moya.git", majorVersion: 9),
-]
-
-let isTest = ProcessInfo.processInfo.environment["TEST"] == "1"
-if isTest {
-  dependencies.append(contentsOf: [
-    .Package(url: "https://github.com/devxoul/Immutable.git", majorVersion: 0),
-    .Package(url: "https://github.com/devxoul/Then.git", majorVersion: 2),
-  ])
-}
 
 let package = Package(
   name: "MoyaSugar",
-  targets: [
-    Target(name: "MoyaSugar"),
+  products: [
+    .library(name: "MoyaSugar", targets: ["MoyaSugar"]),
   ],
-  dependencies: dependencies
+  dependencies: [
+    .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "9.0.0")),
+    .package(url: "https://github.com/devxoul/Immutable.git", .upToNextMajor(from: "0.5.0")),
+    .package(url: "https://github.com/devxoul/Then.git", .upToNextMajor(from: "2.2.0")),
+  ],
+  targets: [
+    .target(name: "MoyaSugar", dependencies: ["Moya"]),
+    .testTarget(name: "MoyaSugarTests", dependencies: ["MoyaSugar", "Immutable", "Then"]),
+  ]
 )
