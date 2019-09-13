0b60fbc203927d56bf20145a70bc0eaa721ff5d7
Suyeol Jeon <devxoul@gmail.com>
2019-09-13 16:45:39 +0900

    Update Package description

--- Package.swift
+++ Package.swift
@@ -1,14 +1,17 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
   name: "MoyaSugar",
+  platforms: [
+    .macOS(.v10_11), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+  ],
   products: [
     .library(name: "MoyaSugar", targets: ["MoyaSugar"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "10.0.0")),
+    .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "14.0.0-beta.2")),
     .package(url: "https://github.com/devxoul/Immutable.git", .upToNextMajor(from: "0.5.0")),
     .package(url: "https://github.com/devxoul/Then.git", .upToNextMajor(from: "2.2.0")),
   ],
