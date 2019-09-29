133f041fb4836db6adb3be8dffd73b8557188f66
Suyeol Jeon <devxoul@gmail.com>
2019-09-30 02:19:10 +0900

    Add MultiSugarTarget

---
+++ Sources/MoyaSugar/MultiSugarTarget.swift
@@ -0,0 +1,33 @@
+import Foundation
+import Moya
+
+public enum MultiSugarTarget: SugarTargetType {
+  case target(SugarTargetType)
+
+  var target: SugarTargetType {
+    switch self {
+    case let .target(target):
+      return target
+    }
+  }
+
+  public var baseURL: URL {
+    return self.target.baseURL
+  }
+
+  public var route: Route {
+    return self.target.route
+  }
+
+  public var parameters: Parameters? {
+    return self.target.parameters
+  }
+
+  public var headers: [String: String]? {
+    return self.target.headers
+  }
+
+  public var sampleData: Data {
+    return self.target.sampleData
+  }
+}
